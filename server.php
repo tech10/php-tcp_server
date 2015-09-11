bd32e6999fbaad13ad2c830a08a44251faaa90f3
tech10 <blakeblind@gmail.com>
2015-09-11 06:41:51 -0600

    Changed where the constants were defined slightly, removed a trivial and unneeded setting for setting execution time limits, and removed the initial log of server events having new lines directly inserted.

--- server.php
+++ server.php
@@ -2,17 +2,17 @@
 <?php
 $load_start = microtime(true);
 declare(ticks=1);
+define('DS', DIRECTORY_SEPARATOR);
+define('NL', PHP_EOL);
 $phpversion_req = '5.4';
 if (version_compare(phpversion(), $phpversion_req, '<'))
-die('You are currently running php version ' . phpversion() . ', and this script requires at least php version ' . $phpversion_req . "\r\n");
-define('DS', DIRECTORY_SEPARATOR);
+die('You are currently running php version ' . phpversion() . ', and this script requires at least php version ' . $phpversion_req . NL);
 $current_dir = __DIR__ . DS;
 chdir($current_dir);
 $user = get_current_user();
 $pid = getmypid();
 $bindaddr = '[::]';
 $port = 6000;
-set_time_limit(0);
 
 //Include needed files.
 foreach (glob($current_dir . "includes" . DS . "*.php") as $inc_file)
@@ -22,16 +22,16 @@ require_once($inc_file);
 
 $server_directory = $current_dir;
 $server_filename = __FILE__;
-server_log(server_name() . " version " . server_version() . ".\r\n", TRUE);
-server_log("Current working directory: " . getcwd() . "\r\n");
-server_log("Running as user: $user.\r\n");
-server_log("Process ID: $pid.\r\n");
+server_log(server_name() . " version " . server_version() . ".", TRUE);
+server_log("Current working directory: " . getcwd());
+server_log("Running as user: $user.");
+server_log("Process ID: $pid.");
 
 $result = server_start($bindaddr, $port);
 if ($result === FALSE)
-die("Error: The server failed to start.\r\n");
+die("Error: The server failed to start." . NL);
 server_log("Server started successfully binding to the address $bindaddr and the port $port.", TRUE);
-server_log("Script loaded in " . round(microtime(true) - $load_start, 3) . " seconds.\r\n");
+server_log("Script loaded in " . round(microtime(true) - $load_start, 3) . " seconds.");
 while (true)
 {
 //Do all the server events.
