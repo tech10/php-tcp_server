ce74a919ec64d1d9c604c439cd631fb6160e7d01
Blake Galbraith <blakeblind@gmail.com>
2015-09-14 04:59:05 -0600

    Changed class name back to tcp_server and rewrote the class loading convention.

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,7 +4,7 @@ foreach (glob(__DIR__ . DS . "functions" . DS . "*.php") as $inc_file)
 require_once($inc_file);
 }
 spl_autoload_register(function($class) {
-require_once(__DIR__ . DS . "classes" . DS . $class . DS . "$class.php");
+require_once(__DIR__ . DS . "classes" . DS . $class . "_class" . DS . $class . "_class.php");
 });
 $plugin_dir = getcwd() . DS . "plugins" . DS;
 if (file_exists($plugin_dir))
